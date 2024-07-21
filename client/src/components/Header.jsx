4040f681e0869997afe3eec732ff080e610e2ebd
Sanyam Garg <sngarg2004@gmail.com>
2024-07-21 11:27:46 +0530

    linking component using ract router dom

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 function Header() {
   return (
     <div className='bg-slate-400'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <Link to="/">
             <div className='text-4xl font-bold'>Auth App</div>
+            </Link>
+            
             <div>
                 <ul className='flex gap-10 text-2xl max-w-2'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Signin</li>
+                    <Link to="/"><li>Home</li></Link>
+                    <Link to="/about"><li>About</li></Link>
+                    <Link to="/signin"><li>Signin</li></Link>
+                    
                 </ul>
             </div>
         </div>

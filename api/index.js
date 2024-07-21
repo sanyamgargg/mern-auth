285a54ef6d4242501e52e8a5d8b579b59405059c
Sanyam Garg <sngarg2004@gmail.com>
2024-07-21 11:55:33 +0530

    Db connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
+import { config } from "dotenv";
 import express from "express" ;
+import mongoose from "mongoose";
+import dotenv from "dotenv" ;
+dotenv.config() ;
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("DB connection successful") ;
+})
+.catch((err)=>{
+    console.log("DB connection Unsuccessful",err)
+})
 
 const app = express() ;
 app.listen(3000,()=>{
